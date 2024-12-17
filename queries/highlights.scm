6c993724ba3499f5c517be641d3c2b15a158bfbe
Hugo Parente Lima <hugo.pl@gmail.com>
2024-12-17 16:46:43 -0300

    Update highlight queries.

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,31 +1,29 @@
-(identifier) @variable
-
-((constant) @constant
- (#match? @constant "^[A-Z\\d_]+$"))
-
 [
   "alias"
+  "annotation"
   "begin"
   "break"
   "case"
-  "class"
-  "def"
   "do"
   "end"
   "ensure"
+  "extend"
   "in"
-  "module"
+  "include"
+  "lib"
   "next"
-  "rescue"
+  "select"
   "then"
-  "unless"
-  "until"
   "when"
-  "while"
 ] @keyword
+["def" "fun"] @keyword.function
+["class" "enum" "module" "struct" "union"] @keyword.type
+["require"] @keyword.import
 ["return" "yield"] @keyword.return
-["if" "else" "elsif"] @keyword.conditional
+["if" "else" "elsif" "unless"] @keyword.conditional
+["while" "until"] @keyword.repeat
 ["rescue"] @keyword.exception
+[(private) (protected) "abstract"] @keyword.modifier
 
 (pseudo_constant) @constant.builtin
 
@@ -36,9 +34,13 @@
 
 (regex) @string.regex
 
+(heredoc_content) @string
+[(heredoc_start) (heredoc_end)] @label
+
 (string_escape_sequence) @escape
 
-[(integer) (float)] @number
+(integer) @number
+(float) @number.float
 
 [(true) (false)] @boolean
 
@@ -73,13 +75,10 @@
   "}"
 ] @punctuation.bracket
 
-(class_def name: [(constant) (generic_type)] @type)
+; Types
+[(constant) (generic_instance_type) (generic_type)] @type
 
-(module_def name: [(constant) (generic_type)] @module)
-
-(struct_def name: [(constant) (generic_type)] @type)
-
-(enum_def name: [(constant)] @type)
+(annotation (constant) @attribute)
 
 (method_def name: [(identifier) (constant)] @function.method)
 
@@ -88,4 +87,7 @@
 [
   (class_var)
   (instance_var)
-] @property
+] @variable.member
+
+; function calls
+(call method: (identifier) @function.call)
