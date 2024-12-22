755033ed9c924256d68668cea75b43a8c18fb8fe
Gabriel Holodak <gthepiper@gmail.com>
2024-12-22 14:33:13 -0500

    Verify literal corpus tests with the Crystal parser

--- test/crystal_parse_corpus.cr
+++ test/crystal_parse_corpus.cr
@@ -7,6 +7,12 @@ require "compiler/crystal/syntax"
 
 require "./s_exp_visitor"
 
+# This script ensures the overall structure of the tree-sitter parse tree matches the Crystal
+# compiler's parse tree. This is an extra layer of verification in two ways:
+#  1. The corpus tests are parsed by the compiler, making sure they are free of syntax errors.
+#  2. Tree-sitter must resolve precedence conflicts and ambiguities in the same way as the compiler.
+
+# A tree-sitter corpus test file
 class CorpusFile
   getter file_path : String | Path
 
@@ -19,7 +25,7 @@ class CorpusFile
     file_content = File.read(@file_path)
     lines = file_content.lines
 
-    sections = lines.slice_after { |line| line.starts_with?("===") }.skip(1)
+    sections = lines.slice_after(&.starts_with?("===")).skip(1)
 
     sections.each_slice(2) do |(header, test_lines)|
       title = header[0].strip
@@ -35,16 +41,34 @@ class CorpusFile
   end
 end
 
-STRIPPED_NODE_NAMES = [
-  # the crystal parser represents strings & chars the same, regardless of whether they're escaped or not
+# These nodes will be removed from the expected output
+STRIPPED_NODES = [
+  # string escapes are interpreted by the parser and not preserved
   "char_escape_sequence",
   "string_escape_sequence",
   "ignored_backslash",
+
+  # regex escapes are interpreted by the parser and not preserved
+  "regex_escape_sequence",
+  "regex_character_class",
+  "regex_special_match",
+  "regex_modifier",
+
+  # heredoc bodies are collapsed by the parser
+  "heredoc_end",
+  "heredoc_content",
+  "heredoc_body",
 
   # comments aren't included in the crystal parser's syntax tree
   "comment",
 ]
 
+# These nodes will be renamed in the expected output
+RENAMED_NODES = {
+  "heredoc_start" => "string",
+}
+
+# A single corpus test example
 class CorpusTest
   getter name : String
   getter tags : Array(String)
@@ -52,13 +76,21 @@ class CorpusTest
   getter expected_output : String
 
   def initialize(@name, @tags, @test_body, @expected_output)
-    STRIPPED_NODE_NAMES.each do |node_name|
+    STRIPPED_NODES.each do |node_name|
       @expected_output = strip_node(@expected_output, node_name)
+    end
+
+    RENAMED_NODES.each do |old_name, new_name|
+      @expected_output = rename_node(@expected_output, old_name, new_name)
     end
   end
 
   def strip_node(string, node_name)
     string.gsub(/\s*\(#{node_name}\)/, "")
+  end
+
+  def rename_node(string, old_name, new_name)
+    string.gsub(/\(#{old_name}\)/, "(#{new_name})")
   end
 
   def runnable?
@@ -98,19 +130,49 @@ class CorpusTest
     end
   end
 
+  # A very basic diff that just compares lines directly
+  def format_diff
+    expected_lines = expected_output.lines
+    actual_lines = crystal_tree.lines
+
+    while expected_lines.size < actual_lines.size
+      expected_lines << ""
+    end
+
+    expected_vs_actual = expected_lines.zip?(actual_lines)
+
+    while !expected_vs_actual.empty?
+      expected, actual = expected_vs_actual.shift
+      if expected == actual
+        puts expected
+      else
+        present = [actual]
+        absent = [expected]
+
+        while !expected_vs_actual.empty? &&
+              expected_vs_actual[0][0] != expected_vs_actual[0][1]
+          expected, actual = expected_vs_actual.shift
+
+          present << actual
+          absent << expected
+        end
+
+        puts present.compact.join("\n").rstrip.colorize.red
+        puts absent.compact.join("\n").rstrip.colorize.green
+      end
+    end
+  end
+
   def run
     if expected_output == crystal_tree
       puts "PASS: #{@name}".colorize.green
       true
     else
       puts "FAIL: #{@name}".colorize.red
-      puts "Expected: #{expected_output.bytesize} bytes"
-      puts expected_output
-      puts "Got: #{crystal_tree.bytesize} bytes"
-      puts crystal_tree
+      format_diff
       false
     end
-  rescue ex : Crystal::SyntaxException
+  rescue ex : Crystal::SyntaxException | NotImplementedError
     puts "FAIL: #{@name}".colorize.red
     puts ex
     false
@@ -118,8 +180,7 @@ class CorpusTest
 end
 
 tests = Dir["test/corpus/**/*.txt"]
-  .map { |file| CorpusFile.new(file).tests }
-  .flatten
+  .flat_map { |file| CorpusFile.new(file).tests }
 
 results = tests.select(&.runnable?).map do |test|
   test.run
@@ -136,10 +197,11 @@ if unverified_tests.size > 0
 end
 
 failures = results.count(false)
+passes = results.count(true)
+
+puts "#{passes} tests passed".colorize.green
 
 if failures > 0
   puts "#{failures} failures".colorize.red
   exit 1
-else
-  puts "All tests passed".colorize.green
 end
--- test/s_exp_visitor.cr
+++ test/s_exp_visitor.cr
@@ -1,11 +1,12 @@
 require "compiler/crystal/syntax"
 
+# Visits a Crystal AST and outputs a parse tree in the same s-exp format used by tree-sitter
 class SExpVisitor < Crystal::Visitor
   include Crystal
 
   @io = IO::Memory.new
   @indent = 0
-  @fields = [] of String
+  @fields = [] of String?
 
   def initialize
   end
@@ -28,14 +29,22 @@ class SExpVisitor < Crystal::Visitor
     print_indent
     @io << active_field << "(" << name
     @indent += 1
+    @fields << nil
   end
 
   def exit_node
     @io << ")"
     @indent -= 1
+    @fields.pop
   end
 
   def visit(node : Expressions)
+    # collapse (expressions (nop)) into (nil)
+    if node.expressions.size == 1 && node.expressions.first.is_a?(Nop)
+      enter_node("nil")
+      return false
+    end
+
     enter_node("expressions")
     true
   end
@@ -44,23 +53,61 @@ class SExpVisitor < Crystal::Visitor
     exit_node()
   end
 
-  def field(name : String)
+  def field(name : String, &)
     @fields << name
     yield
     @fields.pop
   end
 
   def active_field : String
-    if @fields.empty?
+    if @fields.empty? || @fields.last.nil?
       ""
     else
       "#{@fields.last}: "
     end
   end
 
+  #############
+  # Identifiers
+  #############
+
+  def visit(node : Crystal::Path)
+    print_node("constant")
+    false
+  end
+
+  def visit(node : Var)
+    if node.name == "self"
+      print_node("self")
+      return false
+    end
+
+    print_node("identifier")
+    false
+  end
+
+  def visit(node : Self)
+    print_node("self")
+    false
+  end
+
+  def visit(node : InstanceVar)
+    print_node("instance_var")
+    false
+  end
+
+  def visit(node : ClassVar)
+    print_node("class_var")
+    false
+  end
+
   ##########
   # Literals
   ##########
+
+  def visit(node : Nop)
+    false
+  end
 
   def visit(node : NilLiteral)
     print_node("nil")
@@ -95,14 +142,454 @@ class SExpVisitor < Crystal::Visitor
     false
   end
 
+  def visit(node : StringInterpolation)
+    enter_node("string")
+
+    node.expressions.each do |interp|
+      next if interp.is_a?(StringLiteral)
+
+      enter_node("interpolation")
+      interp.accept self
+      exit_node
+    end
+
+    exit_node
+    false
+  end
+
+  def visit(node : SymbolLiteral)
+    print_node("symbol")
+    false
+  end
+
+  def visit(node : RegexLiteral)
+    enter_node("regex")
+
+    if (val = node.value).is_a?(StringInterpolation)
+      val.expressions.each do |interp|
+        next if interp.is_a?(StringLiteral)
+
+        enter_node("interpolation")
+        interp.accept self
+        exit_node
+      end
+    end
+
+    exit_node
+    false
+  end
+
+  ############
+  # Structures
+  ############
+
+  def visit(node : RangeLiteral)
+    enter_node("range")
+
+    field "begin" do
+      node.from.accept self
+    end
+
+    field "operator" do
+      print_node("operator")
+    end
+
+    field "end" do
+      node.to.accept self
+    end
+
+    exit_node
+    false
+  end
+
+  def visit(node : TupleLiteral)
+    enter_node("tuple")
+    true
+  end
+
+  def end_visit(node : TupleLiteral)
+    exit_node
+  end
+
+  def visit(node : NamedTupleLiteral)
+    enter_node("named_tuple")
+
+    node.entries.each do |entry|
+      enter_node("named_expr")
+
+      field "name" do
+        print_node("identifier")
+      end
+
+      entry.value.accept self
+
+      exit_node
+    end
+
+    exit_node
+    false
+  end
+
+  def visit(node : HashLiteral)
+    enter_node("hash")
+
+    node.entries.each do |entry|
+      enter_node("hash_entry")
+
+      entry.key.accept(self)
+      entry.value.accept(self)
+
+      exit_node
+    end
+
+    if (of = node.of)
+      field "of_key" do
+        of.key.accept(self)
+      end
+
+      field "of_value" do
+        of.value.accept(self)
+      end
+    end
+
+    exit_node
+    false
+  end
+
+  def visit(node : ArrayLiteral)
+    enter_node("array")
+
+    node.elements.each(&.accept(self))
+
+    field "of" do
+      if ["::Symbol", "::String"].includes? node.of.to_s
+        # %i and %w arrays inject a "of" node automatically, so ignore it
+        next
+      end
+
+      node.of.try(&.accept(self))
+    end
+
+    exit_node
+    false
+  end
+
+  #####################
+  # Classes and methods
+  #####################
+
+  def visit(node : Def)
+    enter_node("method_def")
+
+    field "visibility" do
+      case node.visibility
+      in .public?
+      in .protected? then print_node("protected")
+      in .private?   then print_node("private")
+      end
+    end
+
+    field "receiver" do
+      node.receiver.try &.accept self
+    end
+
+    field "name" do
+      if operator? node.name
+        print_node("operator")
+      else
+        print_node("identifier")
+      end
+    end
+
+    field "params" do
+      if node.args.size > 0
+        enter_node("param_list")
+        node.args.each &.accept self
+        exit_node
+      end
+    end
+
+    field "type" do
+      node.return_type.try &.accept self
+    end
+
+    field "forall" do
+      # TODO
+    end
+
+    field "body" do
+      body = node.body
+
+      case body
+      when Nop
+        break
+      when Expressions
+        # keep it
+      else
+        body = Expressions.new([body])
+      end
+
+      body.accept self
+    end
+
+    exit_node
+    false
+  end
+
+  def visit(node : Arg)
+    enter_node("param")
+
+    field "extern_name" do
+      if node.external_name != node.name
+        print_node("identifier")
+      end
+    end
+
+    field "name" do
+      print_node("identifier")
+    end
+
+    field "type" do
+      node.restriction.try &.accept self
+    end
+
+    field "default" do
+      node.default_value.try &.accept self
+    end
+
+    exit_node
+    false
+  end
+
+  def visit(node : ProcLiteral)
+    enter_node("proc")
+
+    field "params" do
+      if node.def.args.size > 0
+        enter_node("param_list")
+
+        node.def.args.each &.accept self
+
+        exit_node
+      end
+    end
+
+    field "type" do
+      node.def.return_type.try(&.accept self)
+    end
+
+    field "block" do
+      body = node.def.body
+
+      case body
+      when Nop
+        # keep it
+      when Expressions
+        # keep it
+      else
+        body = Expressions.new([body])
+      end
+
+      enter_node("block")
+
+      field "body" do
+        body.accept self
+      end
+
+      exit_node
+    end
+
+    exit_node
+    false
+  end
+
+  def visit(node : ProcPointer)
+    enter_node("method_proc")
+
+    field "receiver" do
+      node.obj.try &.accept self
+    end
+
+    field "method" do
+      if operator? node.name
+        print_node("operator")
+      else
+        print_node("identifier")
+      end
+    end
+
+    field "params" do
+      if node.args.size > 0
+        enter_node("param_list")
+
+        node.args.each &.accept self
+
+        exit_node
+      end
+    end
+
+    exit_node
+    false
+  end
+
+  def visit(node : ClassDef)
+    enter_node("class_def")
+    # TODO
+    true
+  end
+
+  def end_visit(node : ClassDef)
+    exit_node
+  end
+
+  def visit(node : Block)
+    enter_node("block")
+    # TODO
+    true
+  end
+
+  def end_visit(node : Block)
+    exit_node
+  end
+
+  ##############
+  # Control flow
+  ##############
+
+  def visit(node : If)
+    enter_node("conditional")
+
+    field "cond" do
+      node.cond.accept self
+    end
+
+    field "then" do
+      node.then.accept self
+    end
+
+    field "else" do
+      node.else.accept self
+    end
+
+    exit_node
+    false
+  end
+
+  def visit(node : Return)
+    enter_node("return")
+
+    if (exp = node.exp)
+      enter_node("argument_list")
+      exp.accept self
+      exit_node
+    end
+
+    exit_node
+    false
+  end
+
+  #######
+  # Types
+  #######
+
+  def visit(node : ProcNotation)
+    enter_node("proc_type")
+
+    node.inputs.try(&.each(&.accept self))
+
+    field "return" do
+      node.output.try(&.accept self)
+    end
+
+    exit_node
+    false
+  end
+
+  def visit(node : Union)
+    enter_node("union_type")
+    true
+  end
+
+  def end_visit(node : Union)
+    exit_node
+  end
+
+  def visit(node : Generic)
+    # `a : {Foo}` is represented in Crystal's syntax tree as `a : ::Tuple(Foo)`
+    if node.name.to_s == "::Tuple"
+      enter_node("tuple_type")
+      node.type_vars.each(&.accept self)
+      exit_node
+      return false
+    end
+
+    if node.suffix.asterisk?
+      enter_node("pointer_type")
+      node.type_vars.each(&.accept self)
+      exit_node
+      return false
+    end
+
+    enter_node("generic_instance_type")
+
+    node.name.accept self
+
+    field "params" do
+      enter_node("param_list")
+      node.type_vars.each(&.accept self)
+      # TODO: named args
+      exit_node
+    end
+
+    exit_node
+    false
+  end
+
   ##############
   # Method calls
   ##############
+  OPERATOR_TOKENS = [
+    "+",
+    "-",
+    "*",
+    "/",
+    "//",
+    "%",
+    "&",
+    "|",
+    "^",
+    "**",
+    ">>",
+    "<<",
+    "==",
+    "!=",
+    "<",
+    "<=",
+    ">",
+    ">=",
+    "<=>",
+    "===",
+    "[]",
+    "[]?",
+    "[]=",
+    "!",
+    "~",
+    "!~",
+    "=~",
+    "&+",
+    "&-",
+    "&*",
+    "&**",
+  ]
+
+  def operator?(name : String)
+    OPERATOR_TOKENS.includes? name
+  end
 
   def visit(node : Call)
     named_args = node.named_args
 
-    ident_match = /[0-9A-Za-z_\N{U+00a0}-\N{U+10ffff}]/
+    ident_match = /[0-9A-Za-z_\x{00a0}-\x{10ffff}]/
 
     is_plain_identifier_call = !node.has_parentheses? &&
                                !node.global? &&
@@ -117,27 +604,88 @@ class SExpVisitor < Crystal::Visitor
       # the tree-sitter parser doesn't have semantic context to identify this as a call,
       # so we output as a plain identifier
       enter_node("identifier")
+      exit_node
       return false
     end
 
+    if node.name == "`"
+      # this is a `command` literal
+      enter_node("command")
+
+      node.args.each do |arg|
+        case arg
+        when StringLiteral
+          next
+        when StringInterpolation
+          arg.expressions.each do |interp|
+            next if interp.is_a?(StringLiteral)
+
+            enter_node("interpolation")
+            interp.accept self
+            exit_node
+          end
+        else
+          raise "unexpected node #{arg.class} #{arg}"
+        end
+      end
+
+      exit_node
+      return false
+    end
+
+    unary_tokens = [
+      "~",
+      "+",
+      "-",
+      "&+",
+      "&-",
+    ]
+
+    named_args_size = named_args.try(&.size) || 0
+    has_args = (node.args.size + named_args_size) > 0
+
+    is_operator = operator? node.name
+
+    is_unary_operator = unary_tokens.includes?(node.name) &&
+                        node.obj &&
+                        !has_args &&
+                        named_args_size == 0 &&
+                        !(node.block_arg) &&
+                        !(node.block)
+
     enter_node("call")
 
-    field("reciever") do
+    if is_unary_operator && (obj = node.obj)
+      field "method" { print_node("operator") }
+      field "receiver" { obj.accept self }
+
+      exit_node
+      return false
+    end
+
+    field "receiver" do
       node.obj.try &.accept(self)
     end
 
     field "method" do
-      print_node("identifier")
+      if is_operator
+        print_node("operator")
+      else
+        print_node("identifier")
+      end
     end
 
     field "arguments" do
-      if node.args.size > 0
-        print_node("<todo>")
-      end
+      break if !has_args
+
+      enter_node("argument_list")
+      node.args.each(&.accept self)
 
       if named_args && named_args.size > 0
+        # TODO
         print_node("<todo>")
       end
+      exit_node
     end
 
     field "block" do
@@ -145,14 +693,73 @@ class SExpVisitor < Crystal::Visitor
       node.block.try &.accept(self)
     end
 
+    exit_node
     false
   end
 
-  def end_visit(node : Call)
+  def visit(node : BinaryOp)
+    case node
+    in Or       then enter_node("or")
+    in And      then enter_node("and")
+    in BinaryOp then raise "not possible"
+    end
+
+    node.left.accept self
+    print_node("operator")
+    node.right.accept self
+
     exit_node
+    false
   end
 
+  def visit(node : Assign)
+    enter_node("assign")
+    field "lhs" do
+      node.target.accept(self)
+    end
+    field "rhs" do
+      node.value.accept(self)
+    end
+    exit_node
+    false
+  end
+
+  def visit(node : MultiAssign)
+    enter_node("assign")
+    field "lhs" do
+      node.targets.each &.accept(self)
+    end
+    field "rhs" do
+      node.values.each &.accept(self)
+    end
+    exit_node
+    false
+  end
+
+  def visit(node : TypeDeclaration)
+    enter_node("type_declaration")
+
+    field "var" do
+      node.var.accept self
+    end
+
+    field "type" do
+      node.declared_type.accept self
+    end
+
+    field "value" do
+      node.value.try &.accept self
+    end
+
+    exit_node
+    false
+  end
+
+  ##########
+  # Fallback
+  ##########
+
   def visit(node)
-    raise "visit_node unimplemented for #{node.class}"
+    raise NotImplementedError.new(node.class)
   end
 end
