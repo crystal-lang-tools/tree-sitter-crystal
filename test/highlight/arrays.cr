d8facb1526a74076931e365e4925150396721ccf
Gabriel Holodak <gthepiper@gmail.com>
2025-02-21 15:12:12 -0500

    Highlight 'of' keyword

---
+++ test/highlight/arrays.cr
@@ -0,0 +1,9 @@
+[nil]
+# <-^ punctuation.bracket
+
+[1, 3, 5] of Int32
+# <-    ^          punctuation.bracket
+# ^  ^             punctuation.delimiter
+#^  ^  ^           number
+#         ^^       keyword
+#            ^^^^^ type
