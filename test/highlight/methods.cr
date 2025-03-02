a3511303e61119b781efa7a9a039feee2d5d2184
Gabriel Holodak <gthepiper@gmail.com>
2025-03-02 17:45:50 -0500

    Highlight all method and fun names

--- test/highlight/methods.cr
+++ test/highlight/methods.cr
@@ -1,12 +1,26 @@
 def foo(bar : String, *a : Bool, **nums : Int32, &blk : ->)
-#       ^^^            ^           ^^^^           ^^^ variable.parameter
+#   ^^^                                                     function.method
+#       ^^^            ^           ^^^^           ^^^       variable.parameter
 rescue ex : StandardError
 #      ^^ variable.parameter
 end
 
 def foo(bar, *a, **nums, &blk)
-#       ^^^   ^    ^^^^   ^^^ variable.parameter
+#   ^^^                         function.method
+#       ^^^   ^    ^^^^   ^^^   variable.parameter
 rescue ex
 #      ^^ variable.parameter
 end
 
+abstract def foo()
+#            ^^^    function.method
+
+macro foo(args)
+#     ^^^       function.method
+#         ^^^^  variable.parameter
+end
+
+fun foo = __foo(a : Int32)
+#   ^^^   ^^^^^             function
+#               ^           variable.parameter
+end
